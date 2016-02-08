fa289489649a78ca033f96ee7d74b67519088550
Elechero <davidhervil@hotmail.com>
2016-02-08 18:40:55 -0430

    Update servidor.c

--- Proyecto1/servidor.c
+++ Proyecto1/servidor.c
@@ -18,9 +18,12 @@ void obtener_usuario(char * buffer, char * usuario){
 	usuario=(char *)malloc(i+1);
 	i=0;
 	while(buffer[i]!='\0'){
-		usuario[i]=buffer[i]
+		usuario[i]=buffer[i];
 	}
 	usuario[i]='\0';
+}
+
+void obtener_pipe_r(char * usr, char * piper){
 }
 
 int anhadir_usuario(char * conjunto[], char * usr){
@@ -61,7 +64,7 @@ int main(int argc, char *argv[]){
 		pipe_com = "/tmp/servidor1210761-1210796";
 
 	}else if(argc==2){
-		nam_given_size=strlen(argv[1]);
+		nam_given_size = strlen(argv[1]);
 		pipe_com = (char *)malloc(tmp_part+nam_given_size+1);
 		memcpy(pipe_com,"/tmp/",tmp_part);
 		memcpy(pipe_com + tmp_part,argv[1],nam_given_size+1);
@@ -72,29 +75,39 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	mkfifo(pipe_com,0777);
+	if(mkfifo(pipe_com,0666)<0){
+		fprintf(stderr,"NO SE PUDO CREAR pipe_com\n");
+		return 1;
+	}else{
+		printf("PIpe de coneccion creado\n");
+	}
 	if(com_fd = open(pipe_com,O_RDONLY | O_NONBLOCK)<0){
-		fprintf(stderr, "Error al abrir pipe de comunicacion");
+		fprintf(stderr, "Error al abrir pipe de comunicacion\n");
 		return 1;
 	}
 	FD_SET(com_fd,&comm);
+	printf("Se abrio el pipe: %s\n",pipe_com);
 	while(1){
+		printf("while true\n");
 		//SELECT
 		//Chequear pipe de comunicacion
 		comm_cpy = comm;
-		comm_success = select(2,&comm_cpy,NULL,NULL, &tv);
+		comm_success = select(1,&comm_cpy,NULL,NULL, &tv);
 		if(comm_success == -1){
 			perror("Error de comunicacion");
 		}else if(comm_success){
 			read(com_fd,com_buff,TAM);
+			printf("Recibido: %s\n",com_buff);
+			/*
 			obtener_usuario(com_buff,usuario_aux);
 			obtener_pipe_r(usuario_aux,pipe_r);
 			obtener_pipe_w(usuario,pipe_w);
 			if(!anhadir_usuario(usuarios,usuario)){
 				//enviar al usuario un mensaje de no poder agregar
-			}
+			*/
 		}
 	}
+
 
 	/*int fd;
 	char * myfifo="/tmp/myfifio";
