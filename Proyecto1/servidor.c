e599d092c05343f4121be73b4cc4323bdd929ba2
Elechero <davidhervil@hotmail.com>
2016-02-08 15:22:31 -0430

    Update servidor.c
    Correcciones menores

--- Proyecto1/servidor.c
+++ Proyecto1/servidor.c
@@ -11,7 +11,9 @@
 int main(int argc, char *argv[]){ 
 	char * pipe_com;
 	char * com_buff[TAM];
-	int fd_lectura[20],fd_escritura[20];
+	char * usuarios[20]={NULL};
+	int fd_lectura[20];
+	int fd_escritura[20];
 	int com_fd,comm_success;
 	size_t tmp_part=strlen("/tmp/");
 	size_t nam_given_size;
@@ -29,7 +31,7 @@ int main(int argc, char *argv[]){
 
 	}else if(argc==2){
 		nam_given_size=strlen(argv[1]);
-		pipe_com = malloc(tmp_part+nam_given_size+1);
+		pipe_com = (char *)malloc(tmp_part+nam_given_size+1);
 		memcpy(pipe_com,"/tmp/",tmp_part);
 		memcpy(pipe_com + tmp_part,argv[1],nam_given_size+1);
 
@@ -40,7 +42,7 @@ int main(int argc, char *argv[]){
 
 	mkfifo(pipe_com,0777);
 	com_fd = open(pipe_com,O_RDONLY | O_NONBLOCK);
-	FD_SET(com_fd,comm);
+	FD_SET(com_fd,&comm);
 	while(1){
 		//SELECT
 		//Chequear pipe de comunicacion
@@ -50,8 +52,8 @@ int main(int argc, char *argv[]){
 			perror("Communication Error");
 		}else if(comm_success){
 			read(com_fd,com_buff,TAM);
-			obtener_usuario(com_buff);
-			obtener_pipes(com_buff,pipe_r,pipe_w)
+			//obtener_usuario(com_buff,)
+			//obtener_pipes(com_buff,&pipe_r,&pipe_w)
 		}
 	}
 
